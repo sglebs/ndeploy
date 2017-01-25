978bda164c039b16a5397b27fbb673d6ca26730c
Marcio Marchini <mqm@sglebs.com>
2017-01-25 09:31:21 -0200

    find_packages

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='ndeploy',
     version='0.3',
+    packages=find_packages(),
     description='Deploys n microservices to n PaaS',
     url='https://github.com/sglebs/ndeploy',
     author='Marcio Marchini',

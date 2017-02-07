53ed2f9ed66775cedf8df3d9631214f8015d98d7
Marcio Marchini <mqm@sglebs.com>
2017-02-06 23:05:37 -0200

    wiyth openshift_run.sh

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ndeploy',
-    version='0.5.9',
+    version='0.5.10',
     packages=find_packages(),
     description='Deploys n microservices to n PaaS',
     url='https://github.com/sglebs/ndeploy',

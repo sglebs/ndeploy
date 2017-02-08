9822c3f96814407bf27c8942bf9f643d8b20e84d
Marcio Marchini <mqm@sglebs.com>
2017-02-08 15:58:08 -0200

    does not create 1 app per Procfile entry

--- nd/stackato.py
+++ nd/stackato.py
@@ -38,28 +38,21 @@ def deploy(config_as_dict):
     stackato_login(config_as_dict)
     stackato_create_project_area(config_as_dict)
     stackato_create_empty_apps(config_as_dict)
-    #stackato_configure_created_apps(config_as_dict)
+    stackato_configure_created_apps(config_as_dict)
     stackato_deploy_apps(config_as_dict)
 
 # ------------------------------------------------------------
 
 def stackato_create_empty_apps(config_as_dict):
     for repo_url, branch, app_name, app_props in repo_and_branch_and_app_name_and_app_props_iterator(config_as_dict):
-        for label, cmd_line in procfile_iterator(config_as_dict,dir_name_for_repo(repo_url)):
-            suffix_from_label = label
-            if label == "web":
-                suffix_from_label = ""  # default label, should not be used as suffix
-            cmd = '{cli} -n create-app {appname}{suffixfromlabel} '.format(cli=get_cli_command(config_as_dict),
-                                                        appname=app_name,
-                                                        repourl=get_http_repo_url(repo_url),
-                                                        branch=branch,
-                                                        repofullpath=get_repo_full_path_for_repo_url(repo_url, config_as_dict),
-                                                        suffixfromlabel=suffix_from_label,
-                                                        procfilelabel=label,
-                                                        cmdline=cmd_line)
-            print("Creating app %s:%s: %s \n\n" % (app_name, label, cmd))
-            execute_program_and_print_output(cmd, dir_where_to_run=get_repo_full_path_for_repo_dir_name(
-                dir_name_for_repo(repo_url), config_as_dict))
+        cmd = '{cli} -n create-app {appname} '.format(cli=get_cli_command(config_as_dict),
+                                                    appname=app_name,
+                                                    repourl=get_http_repo_url(repo_url),
+                                                    branch=branch,
+                                                    repofullpath=get_repo_full_path_for_repo_url(repo_url, config_as_dict))
+        print("Creating app %s: %s \n\n" % (app_name, cmd))
+        execute_program_and_print_output(cmd, dir_where_to_run=get_repo_full_path_for_repo_dir_name(
+            dir_name_for_repo(repo_url), config_as_dict))
 
 def stackato_create_project_area(config_as_dict):
     cmd = "%s create-space %s" % (get_cli_command(config_as_dict), get_area_name(config_as_dict))
@@ -95,3 +88,20 @@ def stackato_deploy_apps(config_as_dict):
         cmd = "%s start %s" % (get_cli_command(config_as_dict), app_name)
         print("Starting app: %s" % cmd)
         os.system(cmd)
+
+def stackato_configure_created_apps(config_as_dict):
+    app_names_by_repo_dir_name = {}
+    for repo_url, branch, app_name, app_props in repo_and_branch_and_app_name_and_app_props_iterator(config_as_dict):
+        app_names_by_repo_dir_name[dir_name_for_repo(repo_url)] = app_name
+        stackato_create_apps_env_vars_if_needed(config_as_dict, app_name, app_props)  # env vars AFTER because some slam DATABASE_URL
+        #stackato_configure_domains(config_as_dict, app_name, app_props)
+
+def stackato_create_apps_env_vars_if_needed(config_as_dict, app_name, app_props):
+    if "envs" in app_props:
+        print("Configuring env vars for %s" % app_name)
+        for key, value in app_props["envs"].items():
+            cmd = "%s env-add %s %s \"%s\" " % (get_cli_command(config_as_dict), app_name, key, value)
+            print(cmd)
+            os.system(cmd)
+    else:
+        print("WARNING: NO ENV VARS for %s" % app_name)

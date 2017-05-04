144122c1e5738aa86c636be112adc130c54880ab
Marcio Marchini <mqm@sglebs.com>
2017-05-04 14:20:04 -0300

    initial

---
+++ nd/noop.py
@@ -0,0 +1,14 @@
+def process_args(config_as_dict):
+    return config_as_dict
+
+def clean(config_as_dict):
+    print("Clean, no-op")
+    print(config_as_dict)
+
+def deploy(config_as_dict):
+    print("Deploy, no-op")
+    print(config_as_dict)
+
+def undeploy(config_as_dict):
+    print("Undeploy, no-op")
+    print(config_as_dict)
